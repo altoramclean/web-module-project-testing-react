99a801f356e40c8399a3013d1e0a6d62344655f9
Altora Mclean <altoramclean@gmail.com>
2021-12-13 15:22:32 -0500

    test data structure for testShow

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -6,7 +6,36 @@ import Show from './../Show';
 import Episode from '../Episode';
 import Loading from '../Loading';
 
+const testShow = {
+    name: "Stranger Things",
+    img: "../../../public/stranger_things.png",
+    summary: "Movie summary: After the mysterious and sudden vanishing of a young boy, the people of a small town begin to uncover secrets of a government lab, portals to another world and sinister monsters. The boy's mother (Joyce) desperately tries to find him, convinced he is in grave danger, while the police chief searches for answers",
+    seasons: [
+// Season 1
+{   id: 0,
+    name: "Season 1",
+    episodes: []
+},
 
+// Season 2
+{   id: 1,
+    name: "Season 2",
+    episodes: []
+},
+
+// Season 3
+{   id: 2,
+    name: "Season 3",
+    episodes: []
+},
+
+// Season 4 
+{   id: 3,
+    name: "Season 4",
+    episodes: []
+}
+]
+}
 
 
 
